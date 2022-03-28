a4ba92a4dab84ce0766540e2f7fc9a1af7ee27bb
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2022-03-28 19:03:00 +0300

    Enable cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 
 import connectDB from './db.js';
@@ -10,6 +11,8 @@ import urlRouter from './resources/url/url.route.js';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use(morgan('tiny'));
 
 app.use('/url', urlRouter);
